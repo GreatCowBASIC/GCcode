5e23d90e28677917f63533a026149e7a0a3c7da3
angel-spartan <angelivan_spartan@hotmail.com>
2022-11-17 22:43:07 -0600

    GCcode Release 1.73.1, changed repo links

--- Build/vscode/resources/app/out/vs/base/common/worker/simpleWorker.nls.js
+++ Build/vscode/resources/app/out/vs/base/common/worker/simpleWorker.nls.js
@@ -2,4 +2,4 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls",{"vs/base/common/platform":["_"]});
 
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/378660f7dd07287aa34acf90e8fec157425c1ac8/core/vs\base\common\worker\simpleWorker.nls.js.map
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/14f2d26367b7e8f03ff2352516ba27d6302dd7b1/core/vs\base\common\worker\simpleWorker.nls.js.map
--- Build/vscode/resources/app/out/vs/platform/sharedProcess/electron-browser/sharedProcessWorkerMain.nls.js
+++ Build/vscode/resources/app/out/vs/platform/sharedProcess/electron-browser/sharedProcessWorkerMain.nls.js
@@ -2,4 +2,4 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/define("vs/platform/sharedProcess/electron-browser/sharedProcessWorkerMain.nls",{"vs/base/common/errorMessage":["{0}: {1}","A system error occurred ({0})","An unknown error occurred. Please consult the log for more details.","An unknown error occurred. Please consult the log for more details.","{0} ({1} errors in total)","An unknown error occurred. Please consult the log for more details."]});
 
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/378660f7dd07287aa34acf90e8fec157425c1ac8/core/vs\platform\sharedProcess\electron-browser\sharedProcessWorkerMain.nls.js.map
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/14f2d26367b7e8f03ff2352516ba27d6302dd7b1/core/vs\platform\sharedProcess\electron-browser\sharedProcessWorkerMain.nls.js.map
